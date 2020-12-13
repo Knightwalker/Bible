af52088d5f08ee8c13311772b33aefc6e4955a73
Knightwalker <37214263+Knightwalker@users.noreply.github.com>
2020-12-13 12:07:43 +0200

    edit

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 const fs = require("fs");
-console.log(process.env.APP_ENVIRONMENT);
-console.log('kur');
+
 if (process.env.APP_ENVIRONMENT.trim() == "local") {
   let envObj = JSON.parse(fs.readFileSync("./env.json", {encoding: "utf-8"}));
   process.env["APP_HOST"] = envObj.APP_HOST;
@@ -10,6 +9,6 @@ if (process.env.APP_ENVIRONMENT.trim() == "local") {
 
 } else if (process.env.APP_ENVIRONMENT.trim() == "heroku") {
   process.env["APP_PORT"] = process.env.PORT;
-  // check heroku .env file.
+  // check heroku .env file on herokus platform.
   // heroku loads it automatically.
 }
