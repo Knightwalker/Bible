8a1131dff074f4c3c79390f6e6e63230bdae2d30
Knightwalker <37214263+Knightwalker@users.noreply.github.com>
2021-02-12 18:55:03 +0200

    prepare for heroku deploy

---
+++ config.js
@@ -0,0 +1,19 @@
+const config = {
+  development: {
+    APP_HOST: "localhost",
+    APP_PORT: 3000,
+    //DB_URL: "mongodb://localhost:27017/codegigas",
+    MONGODB_URL: "mongodb+srv://codegigas:f0RXWUeYNePTfnaz@sandbox.7lffu.mongodb.net/codegigas"
+  },
+  production: {
+    APP_HOST: "codegigas.com",
+    APP_PORT: 3000,
+    MONGODB_URL: process.env.MONGODB_URL
+  }
+}
+
+module.exports = {
+  APP_HOST: config[process.env.NODE_ENV].APP_HOST,
+  APP_PORT: config[process.env.NODE_ENV].APP_PORT,
+  MONGODB_URL: config[process.env.NODE_ENV].MONGODB_URL,
+};
