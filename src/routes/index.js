4f112cbdb61074a0fd21ad723119811bfa9a1a76
Knightwalker <37214263+Knightwalker@users.noreply.github.com>
2021-05-15 01:50:28 +0300

    error 403 page

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,10 +5,12 @@ const STATUS_CODES = require("http").STATUS_CODES;
 const generalRoutes = require("./general.js");
 const adminRoutes = require("./admin.js");
 const authRoutes = require("./auth.js");
+const errorRoutes = require("./error.js");
 
 router.use(adminRoutes);
 router.use(authRoutes);
 router.use(generalRoutes);
+router.use(errorRoutes);
 
 // catch 404 and forward to error handler
 router.use("/", (req, res) => {
